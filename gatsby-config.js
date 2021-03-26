ad2babf20f16b91953093a7dc330061d428a4799
Giorgio <giorgio.valbonesi@gmail.com>
2021-03-26 10:53:28 +0100

    Added configuration with graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,10 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Onepage Starter",
+    description: "Minimalistic gatsby starter with tailwind css"
+  },
   plugins: [
     `gatsby-plugin-postcss`,
     {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,28 @@
 import React from "react"
 import Helmet from "react-helmet"
+import { graphql, useStaticQuery } from 'gatsby'
 
 export default function Home() {
-  return <>
-     <Helmet title="onepage-starter">
+  const data = useStaticQuery(graphql`
+    query HelmetQuery {
+      site {
+        siteMetadata {
+          title
+          description
+        }
+      }
+    }`)
+
+    return <>
+    <Helmet title={data.site.siteMetadata.title}>
       <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="description" property="og:description" content="minimal onepage-starter with tailwind css" />
+      <meta name="description" property="og:description" content={data.site.siteMetadata.title} />
     </Helmet>
+
     <div className="h-screen text-center bg-gray-100">
     <h1 className="pt-10 text-xl">Welcome to <b>onepage-starter</b>: a minimalistic gatsby starter.</h1>
     <p>Contains the very basics: Tailwindcss, Helmet, Google analytics.</p>
-
     </div>
     </>
 }
