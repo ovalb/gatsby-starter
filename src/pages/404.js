74a40d7dd9aa6dfb24bbe0783cdd2034bdb7e557
Giorgio <giorgio.valbonesi@gmail.com>
2021-03-26 11:19:16 +0100

    Added Layout component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 export default function Page404() {
-    return <h1 className="h-screen m-auto text-center bg-gray-100 pt-10">404: Page not found bro</h1>
+    return <Layout>
+        <h1><b>404:</b> Page not found bro</h1>
+    </Layout> 
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,9 @@
 import React from "react"
-import Helmet from "react-helmet"
-import { graphql, useStaticQuery } from 'gatsby'
+import Layout from "../components/Layout"
 
 export default function Home() {
-  const data = useStaticQuery(graphql`
-    query HelmetQuery {
-      site {
-        siteMetadata {
-          title
-          description
-        }
-      }
-    }`)
-
-    return <>
-    <Helmet title={data.site.siteMetadata.title}>
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="description" property="og:description" content={data.site.siteMetadata.title} />
-    </Helmet>
-
-    <div className="h-screen text-center bg-gray-100">
+  return <Layout>
     <h1 className="pt-10 text-xl">Welcome to <b>onepage-starter</b>: a minimalistic gatsby starter.</h1>
     <p>Contains the very basics: Tailwindcss, Helmet, Google analytics.</p>
-    </div>
-    </>
+  </Layout>
 }
